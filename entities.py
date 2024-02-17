4f3de0550306cfad739e1bce122e1d71d622d8f1
VelIv88 <pia88@yandex.ru>
2024-02-17 14:56:17 +0600

    new entities has been added

--- entities.py
+++ entities.py
@@ -1,27 +1,73 @@
+class Point:
+    """class describing a point in a Cartesian coordinate system
+
+    Args:
+        x (float): the x coordinate
+        y (float): the y coordinate
+        z (float): the z coordinate
+    """
+    def __init__(self, x: float, y: float, z: float) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
 
 class Material:
+    """class describing the mechanical characteristics of a material
 
-    def __init__(
-            self,
-            young_module: float,
-            poisson_ratio: float, allowable_stress: float
-            ) -> None:
+    Args:
+        name (str): material mark name
+        younge_module (float): Young's modulus of tension of the material
+        poisson_ratio (float): Poisson's ratio of the material
+        allowable_stress (float): Permissible tensile stresses of the material
+    """
+    def __init__(self,
+                 name: str,
+                 young_module: float,
+                 poisson_ratio: float,
+                 allowable_stress: float) -> None:
+        self.name = name
         self.young_module = young_module
         self.poisson_ratio = poisson_ratio
         self.allowable_stress = allowable_stress
 
 
-class Beam:
+class Geomety:
+    """class describing the geometric characteristics of the cross section
 
-    def __init__(self, length: float, form: str, material: Material) -> None:
+    Args:
+        name (str): type of the section shape
+    """
+    def __init__(self, name: str, params: dict) -> None:
+
+        self.name = name
+        self.size_params = params
+
+
+class Beam:
+    """class describing a beam
+
+    Args:
+        length (float): length of beam
+        geom (Geomety): geometry of the cross section
+        material (Material): matherial of beam
+    """
+    def __init__(self,
+                 length: float,
+                 geom: Geomety,
+                 material: Material) -> None:
         self.length = length
-        self.form = form
+        self.geom = geom
         self.material = material
 
 
 class Force:
+    """class for describing mechanical force
 
-    def __init__(self, coordinate: str, value: float, angle: float) -> None:
+    Args:
+        coordinate (Point): coordinates of the application of force
+        value (float): magnitude of the force
+        """
+    def __init__(self, coordinate: Point, value: float, angle: float) -> None:
         self.coord = coordinate
         self.value = value
-        self.angle = angle
